798ab3cf5f298c02b04c880c120e1a5f04217eab
Joseph Archer <joseph.archer@gmail.com>
2018-03-26 17:04:26 -0400

    version 0.7

---
+++ README.md
@@ -0,0 +1,131 @@
+ilxr.py
+=======
+
+Latest version = 0.7
+
+This python script will search imdb for information and save in a format compatible with [Roksbox](http://wilddtech.com/roksbox/home/).
+
+It interactively creates individual xml files for each movie.
+
+It is used mainly on Linux, but should work on other OSes.
+
+Quote from Roksbox:
+>Roksbox is a channel on the Roku Digital Video Player that gives you the ability to play your own videos and movies, listen to your own music, and show your own photographs on your television screen.
+
+ilxr.py will search movies or series. If it finds an existing xml file with the same name as the video, it will skip the video.
+If using the '-s' (--series) option, ilxr.py will ask for the series name, then the season, then the episode.
+So it is best to set your seasons up in seperate folders.
+
+By default, it will take a directory and search imdb for each video and ask you to choose the correct one.
+
+###IMDbPY (required) 
+
+**must be the latest version of python-imdbpy**
+
+###Imagemagick (optional)
+
+If you use the -j option, imagemagick is required, because it uses convert and resizes the image to a specified width.
+
+Examples
+=======
+
+```
+$ ./ilxr.py --help
+usage: ilxr.py [-h] [-v] [-x {write,show,both}] [-r] [-j JPG] [-s SERIES]
+               [-l LENGTH] [-g GENRE] [-y YEAR] [-m MPAA] [-a ACTORS] [-d]
+               [DIR]
+
+Create and maintain individual xml files for Roksbox.
+
+positional arguments:
+  DIR                   quoted movie directory
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         show program's version number and exit
+  -x {write,show,both}, --xml {write,show,both}
+                        [write|show|both] xml files
+  -r, --redo            do all movies (defaults to movies without xml files)
+  -j JPG, --jpg JPG     download jpg poster and resize with convert (-j 200)
+  -s SERIES, --series SERIES
+                        quoted series name (-s "Archer")
+  -l LENGTH, --length LENGTH
+                        overide imdb with "quoted" length
+  -g GENRE, --genre GENRE
+                        overide imdb with "quoted" genre
+  -y YEAR, --year YEAR  overide imdb with "quoted" year
+  -m MPAA, --mpaa MPAA  overide imdb with "quoted" mpaa
+  -a ACTORS, --actors ACTORS
+                        overide imdb with "quoted" actors
+  -d, --debug           print variables for debugging
+
+examples: ilxr.py "DIR", ilxr.py -s "Archer" "DIR/Season"
+
+```
+
+```
+$ ./ilxr.py transit
+directory to scan is:  transit
+directory: transit
+directory: transit/shorts
+--------------------
+Big Buck Bunny
+--------------------
+0 - Big Buck Bunny (2008)
+1 - Buck Rogers in the 25th Century (1979) (TV)
+2 - Uncle Buck (1989)
+3 - Bunny Lake Is Missing (1965)
+4 - "The Big Bang Theory" (2007)
+5 - "Big Brother (II)" (2000)
+6 - Big Fish (2003)
+7 - Buck and the Preacher (1972)
+8 - "Big Brother Brasil" (2002)
+9 - "Big Brother (III)" (2000)
+10 - Big Daddy (1999)
+11 - The Great Buck Howard (2008)
+12 - Big Mommas: Like Father, Like Son (2011)
+13 - Big Momma's House (2000)
+14 - Big Momma's House 2 (2006)
+15 - The Big Lebowski (1998)
+16 - Big Stan (2007)
+17 - The House Bunny (2008)
+18 - The Bugs Bunny/Road-Runner Movie (1979)
+19 - Big (1988)
+
+Which movie?  Enter the movie id, or s to skip, or q to quit.
+```
+
+Example XML
+======
+
+```
+<video>
+<title>Maleficent</title>
+<year>2014</year>
+<genre>Action, Adventure, Family, Fantasy, Romance</genre>
+<mpaa>PG</mpaa>
+<director>Robert Stromberg</director>
+<actors>Angelina Jolie, Elle Fanning, Sharlto Copley</actors>
+<description>A vengeful fairy is driven to curse an infant princess, only to discover that the child may be the one person who can restore peace to their troubled land.</description>
+<length>97</length>
+</video>
+```
+
+###Notes
+
+By default, ilxr.py will only work on following extensions: mp4, m4v, and mkv.
+If you need others, add to the line in the python script:
+
+USE_EXT = ('mp4', 'm4v', 'mkv')
+
+
+Author
+======
+
+Joseph Archer (C) 2014
+
+
+License
+=======
+
+The code is covered by the MIT.
---
+++ ilxr.py
@@ -0,0 +1,310 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+import urllib
+from subprocess import Popen, PIPE
+
+try:
+    import imdb
+except:
+    print """
+
+* IMDbPY needed 
+
+(python-imdbpy from apt-get not working)
+
+(Use dev version from: https://github.com/alberanid/imdbpy)
+
+"""
+    sys.exit(2)
+
+USE_EXT = ('mp4', 'm4v', 'mkv')
+version = '0.7'
+
+i = imdb.IMDb()
+
+
+def get_titles (path):
+  titleslist = []
+  for dirName, subdirList, fileList in os.walk(path):
+    print('directory: %s' % dirName)
+    for fname in fileList:
+        f = fname.rsplit('.',1)
+        name = f[0]
+        ext = f[1]
+        if "]" in name:
+           newname = name.split("] ",1)[1]
+        else:
+           newname = name
+        if ext not in USE_EXT:
+                continue
+        fxml = name + ".xml"
+        fjpg = name + ".jpg"
+        dirPath = dirName.rsplit(path,1)[1]
+        titleslist.append((dirName, fname, newname, fxml, fjpg, dirPath))
+    titleslist.sort()
+  return titleslist
+
+
+def jpgdownload (mid,jf):
+    j = i.get_movie(mid)
+    try:
+      url = j['full-size cover url']
+      if url is not None:
+        wFile = urllib.urlopen(url)
+        jfile = str(jf).encode('ascii','ignore')
+        localFile = open(jfile,'w')
+        localFile.write(wFile.read())
+        wFile.close()
+        localFile.close()
+        print "...downloading jpg"
+        print "...resizing jpg"
+        cmd = ["convert", jfile, "-resize", "62400@", jfile]
+        p = Popen( cmd, stdout=PIPE, stdin=PIPE)
+        (stdout, stderr) = p.communicate()
+        if stderr is not None:
+          print stderr
+      else:
+        print "no full-size cover"
+    except:
+      print "error downloading cover"
+      pass
+    
+
+def get_imdb_id (qid):
+    s_result = i.search_movie(qid)
+    id = 0
+    for movie in s_result:
+     print str(id)+' - '+movie['long imdb title']
+     id = id + 1
+    print
+    response = raw_input('Which movie?  Enter the movie id, or s to skip, or q to quit.'  )
+    if "q" in response:
+     sys.exit(0)
+    elif "s" in response:
+     m1 = None
+    else:
+     m = s_result[int(response)]
+     m1 = m.movieID
+    return m1
+
+
+def get_series_imdb_id (sid):
+    m = i.get_movie(sid)
+    m['kind']
+    i.update(m,'episodes')
+    print
+    gid = 1
+    for s in range(1,len(m['episodes'])+1):
+     print str(gid)+' - Season '+str(s)
+     gid = gid + 1
+    print
+    response = raw_input('Which season?  Enter the season id, or s to skip, or q to quit. '  )
+    if "q" in response:
+     sys.exit(0)
+    elif "s" in response:
+     mm = None
+    else:
+     mm = m['episodes'][int(response)]
+    return mm
+
+
+def list_series_imdb_id (mm,ef):
+    id = 1
+    print "Number of episodes: ", len(mm)
+    print
+    for movie in range(1,len(mm)+1):
+      try:
+        print str(id)+' - '+mm[int(movie)]['title']
+      except:
+        print str(id)+' - unknown'
+        pass
+      id = id + 1
+    print
+    print "--------------------"
+    print ef
+    print "--------------------"
+    response = raw_input('Which episode?  Enter the episode id, or s to skip, or q to quit. '  )
+    if "q" in response:
+     sys.exit(0)
+    elif "s" in response:
+     m2 = None
+    else:
+     m1 = mm[int(response)]
+     m2 = m1.movieID
+    return m2
+
+
+def writeXMLoutput (xml_dict,fil):
+    myFile = open(fil, 'w')
+    myFile.write(xml_dict['vid_beg']+'\n')
+    myFile.write(xml_dict['title']+'\n')
+    myFile.write(xml_dict['year']+'\n')
+    myFile.write(xml_dict['genre']+'\n')
+    myFile.write(xml_dict['mpaa']+'\n')
+    myFile.write(xml_dict['director']+'\n')
+    myFile.write(xml_dict['actors']+'\n')
+    myFile.write(xml_dict['description']+'\n')
+    myFile.write(xml_dict['length']+'\n')
+    myFile.write(xml_dict['vid_end']+'\n')
+    myFile.close()
+
+
+def showXMLoutput (xml_dict,fil):
+    print "--------------------"
+    print "should write to: ", fil
+    print "--------------------"
+    print xml_dict['vid_beg']
+    print xml_dict['title']
+    print xml_dict['year']
+    print xml_dict['genre']
+    print xml_dict['mpaa']
+    print xml_dict['director']
+    print xml_dict['actors']
+    print xml_dict['description']
+    print xml_dict['length']
+    print xml_dict['vid_end']
+
+
+def create_xml (imdb_id):
+    xml_dict = {'vid_beg':'<video>'}
+    xml_dict['title'] = '<title></title>'
+    xml_dict['year'] = '<year></year>'
+    xml_dict['genre'] = '<genre></genre>'
+    xml_dict['mpaa'] = '<mpaa></mpaa>'
+    xml_dict['director'] = '<director></director>'
+    xml_dict['actors'] = '<actors></actors>'
+    xml_dict['description'] = '<description></description>'
+    xml_dict['length'] = '<length></length>'
+    xml_dict['vid_end'] = '</video>'
+#
+    if imdb_id is not None:
+        m = i.get_movie(imdb_id)
+        xml_dict['title'] = "<title>"+m['title']+"</title>"
+        try:
+          if args.year:
+            xml_dict['year'] = "<year>"+args.year+"</year>"
+          else:
+            xml_dict['year'] = "<year>"+str(m['year'])+"</year>"
+        except:
+          pass
+        try:
+          if args.genre:
+            xml_dict['genre'] = "<genre>"+args.genre+"</genre>"
+          else:
+            xml_dict['genre'] = "<genre>%s</genre>" %', '.join(m.get('genre'))
+        except:
+          pass
+        try:
+          if args.mpaa:
+            xml_dict['mpaa'] = "<mpaa>"+args.mpaa+"</mpaa>"
+          else:
+            for region in m['certificates']:
+              if region.split(':')[0]=='USA':
+                xml_dict['mpaa'] = "<mpaa>"+region.split(':')[1].encode()+"</mpaa>"
+        except:
+          pass
+        try:
+          xml_dict['director'] = "<director>%s</director>" % ', '.join([director.get('name') for director in (m.get('director') or [])])
+        except:
+          pass
+        try:
+          if args.actors:
+            xml_dict['actors'] = "<actors>"+str(args.actors)+"</actors>"
+          else:
+            actorList = ''
+            for actor in [0,1,2]:
+              actorList = actorList+str(m['actors'][actor])+', '
+            xml_dict['actors'] = "<actors>"+actorList[0:-2]+"</actors>"
+        except:
+          pass
+        try:
+          plot = m['plot outline'].replace(u' \xbb', u'').encode()
+          plot = plot.replace(' |', '')
+          xml_dict['description'] = "<description>"+plot+"</description>"
+        except:
+          pass
+        try:
+          if args.length:
+            xml_dict['length'] = "<length>"+args.length+"</length>"
+          else:
+            xml_dict['length'] = "<length>"+m['runtime'][0]+"</length>"
+            for utime in m['runtime']:
+              if utime.split(':')[0]=='USA':
+                xml_dict['length'] = "<length>"+utime.split(':')[1].encode()+"</length>"
+        except:
+          pass
+    return xml_dict  
+
+
+def pullittogether(mmm,l):
+    if args.series:
+      if mmm is None:
+        sid = get_imdb_id(args.series)
+        mmm = get_series_imdb_id(sid)
+      imdb_id = list_series_imdb_id(mmm,l[2])
+    else:
+      imdb_id = get_imdb_id(l[2])
+    if imdb_id is not None:
+      xml_dict = create_xml(imdb_id)
+      if args.xml == 'show':
+        print "show xml only"
+        showXMLoutput(xml_dict,l[0]+'/'+l[3])
+      elif args.xml == 'write':
+        print "write xml only"
+        writeXMLoutput(xml_dict,l[0]+'/'+l[3])
+      else:
+        print "both show and write xml"
+        showXMLoutput(xml_dict,l[0]+'/'+l[3])
+        writeXMLoutput(xml_dict,l[0]+'/'+l[3])
+      if args.jpg:
+        jfile = l[0]+'/'+l[4]
+        jpgdownload(imdb_id,jfile)
+    return mmm
+
+
+def main():
+    mmm = None
+    print "directory to scan is: ", args.dirfile
+    thelist = get_titles(args.dirfile)
+    for l in thelist:
+       print "--------------------"
+       print l[2]
+       print "--------------------"
+       xfile = l[0]+'/'+l[3]
+       if os.path.isfile(xfile):
+         if args.new:
+           continue
+         else:
+           with open(xfile) as f: 
+             print f.read()
+           print "--------------------"
+           response = raw_input('Change XML file? Enter y to change, or q to quit, or enter to continue without changing.'  )
+           if "q" in response:
+             sys.exit(0)
+           elif "y" in response:
+             mmm = pullittogether(mmm,l)
+             continue
+           else:
+             continue
+       pullittogether(mmm,l)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Create and maintain xlm files for Roksbox.', epilog='examples: ilxr.py "DIR", ilxr.py -s "Archer" "DIR"')
+    parser.add_argument('dirfile', metavar='DIR', type=str, nargs='?', help='quoted movie directory', default='.')
+    parser.add_argument('-x','--xml', action='store', type=str, help='[write|show|both] xml files', choices=['write','show','both'], default='write')
+    parser.add_argument('-n','--new', action='store_true', help='only do movies without xml files')
+    parser.add_argument('-j','--jpg', action='store_true', help='download jpg poster of video and resize with convert')
+    parser.add_argument('-s','--series', action='store', help='quoted series name')
+    parser.add_argument('-l','--length', action='store', help='overide imdb with "quoted" length')
+    parser.add_argument('-g','--genre', action='store', help='overide imdb with "quoted" genre')
+    parser.add_argument('-y','--year', action='store', help='overide imdb with "quoted" year')
+    parser.add_argument('-m','--mpaa', action='store', help='overide imdb with "quoted" mpaa')
+    parser.add_argument('-a','--actors', action='store', help='overide imdb with "quoted" actors')
+    args = parser.parse_args()
+
+    main()
